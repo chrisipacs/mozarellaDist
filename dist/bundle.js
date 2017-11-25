e7d6e3354e80e6d6f72be7efddd3d73b84abea77
chrisipacs <pitypalattysama@gmail.com>
2017-11-25 21:53:48 +0000

    create new build

--- dist/bundle.js
+++ dist/bundle.js
@@ -26063,7 +26063,7 @@
 	                    null,
 	                    _react2.default.createElement(_reactContenteditable2.default, {
 	                        name: 'title',
-	                        html: that.state.list.name // innerHTML of the editable div
+	                        html: this.state.enableEditing // innerHTML of the editable div
 	                        , disabled: !this.state.enableEditing // use true to disable edition
 	                        , onChange: this.updateListName // handle innerHTML change
 	                    })
